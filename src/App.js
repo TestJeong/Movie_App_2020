3bb6c0a1a5af34f8010bd9320d554419a09adada
TestJeong <jyunj3015@gmail.com>
2020-07-01 22:23:39 +0900

    axios API 사용
    - API 사용 영화 리스트 불러옴

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({
+      movies,
+      isLoading: false,
+    }); // movies : movies 라는 뜻. 즉 state에 있는 기본값 movies 안에 api로 받아온 movies 배열 리스트를 넣는다는 뜻
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
-  }
+    this.getMovies();
+  } // render 된 즉후 실행됨.
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready!"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary}</p>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  ); //map 메서드에서 index는 genre의 number를 부여한다 이름은 어떤 이름이든 상관 없음
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired, // string 타입으로 되어있는 배열을 검사함
+};
+
+Movie.defaultProps = {
+  genres: [],
+};
+export default Movie;
